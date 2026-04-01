84be50a215ec521cc7793f4c07e35f8d57b0bd36
Hasibul-Islam-Shanto <mdhasibulislamshanto007@gmail.com>
2026-04-01 12:30:23 +0600

    feat(app): add changes on readme file

--- components/header-profile-button.tsx
+++ components/header-profile-button.tsx
@@ -1,21 +1,11 @@
 'use client';
 import LoginButton from '@/components/login-button';
 import { SignedOut, UserButton } from '@clerk/nextjs';
-import { User } from 'lucide-react';
 
 const HeaderProfileButton = () => {
   return (
     <>
-      <UserButton>
-        <UserButton.MenuItems>
-          <UserButton.Link
-            label="Profile"
-            labelIcon={<User className="size-4" />}
-            href="/profile"
-          />
-        </UserButton.MenuItems>
-      </UserButton>
-
+      <UserButton />
       <SignedOut>
         <LoginButton />
       </SignedOut>
